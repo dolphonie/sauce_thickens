a9743170e0c97b9e82807beee7361a0503bac7e3
Lilian Luong <lilianluong64@gmail.com>
2022-02-05 22:59:56 -0500

    web api setup

--- web/server/api.ts
+++ web/server/api.ts
@@ -15,6 +15,26 @@ const router = express.Router();
 // | write your API methods below!|
 // |------------------------------|
 
+let speed = 0;
+
+// route for handling input from web client
+router.post("/input", (req, res) => {
+  if (req.body.speed) speed = req.body.speed;
+  res.send(speed.toString());
+});
+
+// route for handling device polling requests
+router.get("/status", (req, res) => {
+  res.send(speed.toString());
+});
+
+// route for handling device events
+router.post("/interrupt", (req, res) => {
+  speed = 0;
+  // Handle some event
+  res.send(speed.toString());
+});
+
 // anything else falls to this "not found" case
 router.all("*", (req, res) => {
   const msg = `Api route not found: ${req.method} ${req.url}`;
